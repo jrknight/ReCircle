8afa873654607b1e1fa304802767132bb32486f4
Elijah Boucahrd <elijahboucharddrhs@gmail.com>
2018-10-13 11:17:17 -0700

    URL Image working

--- ReCircle/DummyData.cs
+++ ReCircle/DummyData.cs
@@ -13,12 +13,12 @@ namespace ReCircle
 
         public static void Setup()
         {
-            Instance = new DummyData { BrowsePage = new List<Item> { new Item { Title = "Item", OwnerNickname = "Josh", Description = "Like new", CurrentHolderNickname = "Elijah", PhotoUrl = "photos.google.com" },
-            new Item{ Title = "Shoes", OwnerNickname= "Colton", Description = "Used, no use for them now", CurrentHolderNickname = "Colton", PhotoUrl = "photos.google.com" },
-            new Item { Title = "Sunglasses", OwnerNickname = "Denielle", Description = "No scratches, polarized", CurrentHolderNickname = "Elijah", PhotoUrl = "photos.google.com" },
-            new Item { Title = "Hat", OwnerNickname = "Josh", Description = "Looks fine, works like a hat", CurrentHolderNickname = "Josh", PhotoUrl = "photos.google.com" },
-            new Item { Title = "Coffee Mug", OwnerNickname = "Denielle", Description = "Its a coffee mug", CurrentHolderNickname = "Dave! Yognaught", PhotoUrl = "photos.google.com" },
-            new Item { Title = "HydroFlask", OwnerNickname = "Josh", Description = "Like new", CurrentHolderNickname = "Elijah", PhotoUrl = "photos.google.com" } } };
+            Instance = new DummyData { BrowsePage = new List<Item> { new Item { Title = "Item", OwnerNickname = "Josh", Description = "Like new", CurrentHolderNickname = "Elijah", PhotoUrl = new Uri("https://media03.toms.com/static/www/images/product/MENS/ATG/SIDE/10011588-GreyLinenMensPreston-P-1450x1015.jpg") },
+            new Item{ Title = "Shoes", OwnerNickname= "Colton", Description = "Used, no use for them now", CurrentHolderNickname = "Colton", PhotoUrl = new Uri("https://cdn.shopify.com/s/files/1/0898/5824/products/QUAY_HIGHKEY_BLACK_FADE_FRONT_1024x1024.jpg?v=1537995892") },
+            new Item { Title = "Sunglasses", OwnerNickname = "Denielle", Description = "No scratches, polarized", CurrentHolderNickname = "Elijah", PhotoUrl = new Uri("https://cdn.shopify.com/s/files/1/0898/5824/products/QUAY_HIGHKEY_BLACK_FADE_FRONT_1024x1024.jpg?v=1537995892") },
+            new Item { Title = "Hat", OwnerNickname = "Josh", Description = "Looks fine, works like a hat", CurrentHolderNickname = "Josh", PhotoUrl = new Uri("https://media03.toms.com/static/www/images/product/MENS/ATG/SIDE/10011588-GreyLinenMensPreston-P-1450x1015.jpg") },
+            new Item { Title = "Coffee Mug", OwnerNickname = "Denielle", Description = "Its a coffee mug", CurrentHolderNickname = "Dave! Yognaught", PhotoUrl = new Uri("https://cdn.shopify.com/s/files/1/0898/5824/products/QUAY_HIGHKEY_BLACK_FADE_FRONT_1024x1024.jpg?v=1537995892") },
+            new Item { Title = "HydroFlask", OwnerNickname = "Josh", Description = "Like new", CurrentHolderNickname = "Elijah", PhotoUrl = new Uri("https://cdn.shopify.com/s/files/1/0898/5824/products/QUAY_HIGHKEY_BLACK_FADE_FRONT_1024x1024.jpg?v=1537995892") } } };
         }
 
         public List<Item> BrowsePage = new List<Item>();
